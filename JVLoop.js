5258f747d7c6d96f2f78e5982f07ea1d526a58e2
G.WilliamBuryat Taka <93561957+WillSnakeTaka@users.noreply.github.com>
2025-12-05 12:17:14 -0500

    Fix loop syntax and add FizzBuzz logic

--- JVLoop.js
+++ JVLoop.js
@@ -8,3 +8,11 @@ for (let i = 1; i<=100; i++){
     console.log(i);
   }
 }
+
+
+for (let i-1; i<=100; i++){
+  let output ="";
+  if (i%3 ===0) output +="Fizz";
+  if (i%5===0) output ="Buzz";
+  console.log(output||i);
+}
